0c7dcb11be76679ec5ef19257eeef4c7fea159ca
Hugo Torres <hugo@user1s-mbp.lan>
2020-10-18 18:39:24 -0400

    Fix babel config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -73,25 +73,6 @@ module.exports = function (webpackEnv) {
                             include: paths.appSrc,
                             loader: require.resolve("babel-loader"),
                             options: {
-                                customize: require.resolve(
-                                    "babel-preset-react-app/webpack-overrides"
-                                ),
-
-                                plugins: [
-                                    [
-                                        require.resolve(
-                                            "babel-plugin-named-asset-import"
-                                        ),
-                                        {
-                                            loaderMap: {
-                                                svg: {
-                                                    ReactComponent:
-                                                        "@svgr/webpack?-svgo,+titleProp,+ref![path]",
-                                                },
-                                            },
-                                        },
-                                    ],
-                                ],
                                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                                 // It enables caching results in ./node_modules/.cache/babel-loader/
                                 // directory for faster rebuilds.
